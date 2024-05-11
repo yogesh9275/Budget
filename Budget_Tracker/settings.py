fb26ef1f1d6c4ceaf33116d53c17b4f7d1746e24
Yogesh Rudra <52611925+yogesh9275@users.noreply.github.com>
2024-05-11 16:39:16 +0530

    Update settings.py

--- Budget_Tracker/settings.py
+++ Budget_Tracker/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'Budget_Tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'MZDjHfTLMvpkmfSOmKzyMlJylWKFqfMC',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '55994',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'MZDjHfTLMvpkmfSOmKzyMlJylWKFqfMC',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '55994',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
