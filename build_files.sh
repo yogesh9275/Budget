f178afd1c49808d7850373f58ef139d017e16cd3
Yogesh Rudra <52611925+yogesh9275@users.noreply.github.com>
2024-05-11 16:26:15 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-python3.9 -m pip install -r requirements.txt 
-python3.9 -m manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic
